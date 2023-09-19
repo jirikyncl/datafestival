8b5fc214413ea67087fc81ee4087ebe9e8b88419
jiri kyncl <jiri.kyncl@movisio.com>
2023-09-19 07:02:37 +0200

    Update selects

--- prepare_base_views.sql
+++ prepare_base_views.sql
@@ -11,9 +11,16 @@ select
     m.name,
     st_transform(m.definition_point, 4326) definition_point,
     st_transform(m.coordinates, 4326) coordinates,
-    c.crime_18_count crime_count,
+    c.crime_count,
     c.year,
     p.person_count
-from crime c
+from
+    (
+        select
+             municipality_code,
+             sum(crime_18_count) crime_count,
+             "year"
+        from crime group by municipality_code, "year"
+    ) c
     join municipality m on c.municipality_code = m.code
     join population p on c.municipality_code = p.municipality_code and c.year = p.year;
